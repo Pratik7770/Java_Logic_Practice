44a30fe84c0c20f99d2949e67e7491733b6fd6d0
Pratik7770 <pratikpawar7770@gmail.com>
2026-01-10 15:34:25 +0530

    Even & Odd Added

---
+++ logicbuilding/One.java
@@ -0,0 +1,19 @@
+package logicbuilding;
+
+//Event And OddS
+
+public class One {
+	
+	public static void main(String[] args) 
+	{
+		int number =70;
+		if ((number/2)*2==number)
+		{
+			System.out.println("number is even");
+			
+		} else {
+			System.out.println("number is odd");
+		}	
+	}
+
+}
